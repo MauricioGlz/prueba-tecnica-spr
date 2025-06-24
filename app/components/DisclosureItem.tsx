ef83685568f28bba0695db84d72b717349ec92d3
MauricioGlz <mauricioandres288@gmail.com>
2025-06-23 19:32:56 -0600

    Fixed UI details

--- app/components/DisclosureItem.tsx
+++ app/components/DisclosureItem.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 export default function DisclosureItem({ dayData, dayIndex }) {
   
   const { Dia, fecha, eventos = [] } = dayData || {}; 
+  
   const isDayIndexEven = dayIndex % 2 === 0;
 
   const evenPanelBg = 'bg-[#F8E3D4]'; 
@@ -18,6 +19,8 @@ export default function DisclosureItem({ dayData, dayIndex }) {
  
   const panelBgClass = isDayIndexEven ? evenPanelBg : oddPanelBg;
   const textColorClass = isDayIndexEven ? evenTextColor : oddTextColor;
+  const sheduleColorClass = isDayIndexEven ? 'bg-[#098879]' : 'bg-[#fae4d3]';
+  const sheduleTextColorClass = isDayIndexEven ? 'text-white' : 'text-black';
 
   return (
     <Disclosure>
@@ -35,53 +38,62 @@ export default function DisclosureItem({ dayData, dayIndex }) {
             transition
             className={`px-4 pt-4 pb-2 text-gray-500 origin-top transition duration-200 ease-out data-[closed]:-translate-y-6 data-[closed]:opacity-0 ${panelBgClass}`}        
           >
-            {eventos.map((evento, index) => (
 
-              <div key={index} className="data-container grid md:grid-cols-2 sm:grid-cols-1 gap-4 py-6 border-b border-gray-300 last:border-b-0">
-                {/* Primera columna */}
-                <div className="mb-4 order-2 md:order-1"> 
-                  <div className="flex flex-col gap-4"> 
+            {eventos.map( (evento, index) => (
 
-                    {evento.Horario && evento.Horario.lengh > 0 && (
-                      <div className="ml-20 bg-[#098879] w-[114px] h-auto p-2 rounded-r-full font-bold text-white text-2xl">
-                        {evento.Horario}
+              <div>
+
+                <div key={index} className="data-container grid md:grid-cols-2 sm:grid-cols-1 gap-4 py-6">
+                  {/* Primera columna */}
+                  <div className="mb-4 order-2 md:order-1"> 
+                    <div className="flex flex-col gap-4"> 
+
+                      {evento.Horario &&  (
+                        <div className= {`ml-20  w-[114px] h-auto p-2 rounded-r-full font-bold ${sheduleTextColorClass} text-2xl ${sheduleColorClass}`}>
+                          {evento.Horario}
+                        </div>
+                      )}
+
+                      {evento.Show && evento.Show.length > 0 && ( 
+                        <div className="pl-20 mr-10 bg-[#d89658] py-2">
+                            <h1 className="font-medium text-4xl text-[#353036]">{evento.Show}</h1>
+                        </div>
+                      )}  
+
+                      <div className="md:pl-20">
+                        <h1 className="text-[#d89558] font-bold text-4xl">{evento.Titulo}</h1> 
+                        <div className="flex flex-row border-b border-b-2 border-[#d89651] py-2 text-lg font-bold text-[#353036]"> 
+                          {
+                            evento.Tipo.split(',').map((type, typeIndex) => {
+                              return (<div className= {`last:border-l last:border-[#d89651] px-2 last:border-l-2 ${textColorClass} `}> {type}</div> )
+                            })
+                          }
+                        </div>
                       </div>
-                    )}
 
-                    {evento.Show && evento.Show.length > 0 && ( 
-                      <div className="pl-20 mr-10 bg-[#d89658] py-2">
-                          <h1 className="font-medium text-4xl text-[#353036]">{evento.Show}</h1>
+                      <div className="md:pl-20 text-lg font-semibold text-[#d89658]">
+                        <div>Dirección: <span className={`${textColorClass}`} >{evento.Direccion}</span></div>
+                        <div>Año: <span className={`${textColorClass}`} >{evento.Año}</span></div>
+                        <div>Idioma: <span className={`${textColorClass}`} >{evento.Idioma}</span></div>
+                        <div>País: <span className={`${textColorClass}`} >{evento.Pais}</span></div>
+                        <div>Duración: <span className={`${textColorClass}`} >{evento.Duracion}</span></div>
                       </div>
-                    )}  
 
-                    <div className="md:pl-20">
-                      <h1 className="text-[#d89558] font-bold text-4xl">{evento.Titulo}</h1> 
-                      <div className="flex flex-row border-b border-[#d89658] py-2 text-lg font-bold text-[#353036]"> 
-                        <div className= {`border-r border-[#d89658] pr-2  ${textColorClass}`}>{evento.Tipo}</div> 
-                      </div>
                     </div>
-
-                    <div className="md:pl-20 text-lg font-semibold text-[#d89658]">
-                      <div>Dirección: <span className={`${textColorClass}`} >{evento.Direccion}</span></div>
-                      <div>Año: <span className={`${textColorClass}`} >{evento.Año}</span></div>
-                      <div>Idioma: <span className={`${textColorClass}`} >{evento.Idioma}</span></div>
-                      <div>País: <span className={`${textColorClass}`} >{evento.Pais}</span></div>
-                      <div>Duración: <span className={`${textColorClass}`} >{evento.Duracion}</span></div>
-                    </div>
-
-                    <div className="md:pl-20">
-                      <p><b className='text-[#d89658]'>Sinopsis:</b><span className={`${textColorClass}`}> {evento.Sinopsis} </span> </p>
-                    </div>
-
                   </div>
+
+                  {/* Segunda columna */}
+                  <div className='order-1 md:order-2'>
+                  <Image src={evento.Poster} alt="Poster" width={400} height={300} className='w-full px-12'/> 
+                  </div>
+
                 </div>
 
-                {/* Segunda columna */}
-                <div className='order-1 md:order-2'>
-                 
-                 <Image src={evento.Poster} alt="Poster" width={400} height={300} className='w-full'/> 
+                <div className="md:pl-20 mb-10">
+                  <p><b className='text-[#d89658]'>Sinopsis:</b><span className={`${textColorClass}`}> {evento.Sinopsis} </span> </p>
                 </div>
               </div>
+              
             ))}
           </DisclosurePanel>
         </div>
